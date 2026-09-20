caf25ae60362dbb84d057b768c3be20ab51be676
studio1111 <www.studio1111@gmail.com>
2026-09-20 06:29:07 +0330

    build: use Khayyaton keystore by default

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
 
   signingConfigs {
     create("release") {
-      val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/my-upload-key.jks"
+      val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/khayyaton.jks"
       storeFile = file(keystorePath)
       storePassword = System.getenv("STORE_PASSWORD")
       keyAlias = "khayyaton"
