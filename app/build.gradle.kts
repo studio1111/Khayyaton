bdd4aa6c1da31ba7e9e5db27d05126fb99144dfa
studio1111 <www.studio1111@gmail.com>
2026-09-20 06:29:46 +0330

    build: enable Firebase task coroutine await

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -113,6 +113,7 @@ dependencies {
   implementation(libs.firebase.appcheck.recaptcha)
   implementation(libs.kotlinx.coroutines.android)
   implementation(libs.kotlinx.coroutines.core)
+  implementation(libs.kotlinx.coroutines.play.services)
   implementation(libs.logging.interceptor)
   implementation(libs.moshi.kotlin)
   implementation(libs.okhttp)
