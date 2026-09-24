9cef3fdf42def58c4d471210299353ed7f6194cb
studio1111 <www.studio1111@gmail.com>
2026-09-24 08:14:34 +0330

    fix: compile against Android API 37 required by current AndroidX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 android {
   namespace = "com.example"
-  compileSdk { version = release(36) { minorApiLevel = 1 } }
+  compileSdk = 37
 
   defaultConfig {
     applicationId = "com.farsinnov.khayyaton"
