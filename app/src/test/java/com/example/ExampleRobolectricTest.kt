1492d90185e1e1a696c4ca35b9c42429ffab1492
studio1111 <www.studio1111@gmail.com>
2026-09-25 10:48:12 +0330

    test: use stable Robolectric SDK 35

--- app/src/test/java/com/example/ExampleRobolectricTest.kt
+++ app/src/test/java/com/example/ExampleRobolectricTest.kt
@@ -10,7 +10,7 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
-@Config(sdk = [36])
+@Config(sdk = [35])
 class ExampleRobolectricTest {
 
   @Test
